9a5f1064ff8c5733d9db90a546fa6e2c04a6a523
Number5IsAlive <james.s.mason@hotmail.com>
2022-05-09 13:51:29 +0100

    20 Selecting Multiple Units

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,14 +1,19 @@
+using Mirror;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
+    [SerializeField] private RectTransform unitSelectionArea = null;
     [SerializeField] private LayerMask layerMask;
 
+    private RTSPlayer player = null;
     private Camera mainCamera = null;
+
+    private Vector2 startPosition;
+
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
 
     private void Awake()
@@ -18,36 +23,85 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update()
     {
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            foreach (Unit selectedUnit in SelectedUnits)
-            {
-                selectedUnit.Deselect();
-            }
-
-            SelectedUnits.Clear();
+            StartSelectionArea();
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
         }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
+    }
+
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in SelectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+
+        SelectedUnits.Clear();
+
+        unitSelectionArea.gameObject.SetActive(true);
+        startPosition = Mouse.current.position.ReadValue();
+
+        UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight));
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2, areaHeight / 2);
     }
 
     private void ClearSelectionArea()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        unitSelectionArea.gameObject.SetActive(false);
 
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
-
-        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
-
-        if (!unit.hasAuthority) { return; }
-
-        SelectedUnits.Add(unit);
-
-        foreach (Unit selectedUnit in SelectedUnits)
+        if (unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
+
+            if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+
+            if (!unit.hasAuthority) { return; }
+
+            SelectedUnits.Add(unit);
+
+            foreach (Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Select();
+            }
+        }
+
+        Vector2 min = unitSelectionArea.anchoredPosition - (unitSelectionArea.sizeDelta / 2);
+        Vector2 max = unitSelectionArea.anchoredPosition + (unitSelectionArea.sizeDelta / 2);
+
+        foreach (Unit unit in player.GetMyUnits())
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
+
+            if (screenPosition.x > min.x && screenPosition.x < max.x && screenPosition.y > min.y && screenPosition.y < max.y)
+            {
+                SelectedUnits.Add(unit);
+                unit.Select();
+            }
         }
     }
 }
